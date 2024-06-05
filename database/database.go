4a7da3d9603a4e94314ddfe2a4b4f4abc258eae2
rrm003 <rammankar003@gmail.com>
2024-06-05 13:55:20 -0500

    refactored database methods

--- database/database.go
+++ database/database.go
@@ -1,4 +1,3 @@
-// database/database.go
 package database
 
 import (
@@ -10,9 +9,46 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+// var DB Database
 
-func InitDatabase() {
+type Database interface {
+	Create(value interface{}) *gorm.DB
+	Where(query interface{}, args ...interface{}) *gorm.DB
+	First(out interface{}, where ...interface{}) *gorm.DB
+	Find(out interface{}, where ...interface{}) *gorm.DB
+	Save(value interface{}) *gorm.DB
+	Delete(value interface{}, where ...interface{}) *gorm.DB
+}
+
+type GormDatabase struct {
+	DB *gorm.DB
+}
+
+func (g *GormDatabase) Create(value interface{}) *gorm.DB {
+	return g.DB.Create(value)
+}
+
+func (g *GormDatabase) Where(query interface{}, args ...interface{}) *gorm.DB {
+	return g.DB.Where(query, args...)
+}
+
+func (g *GormDatabase) First(out interface{}, where ...interface{}) *gorm.DB {
+	return g.DB.First(out, where...)
+}
+
+func (g *GormDatabase) Find(out interface{}, where ...interface{}) *gorm.DB {
+	return g.DB.Find(out, where...)
+}
+
+func (g *GormDatabase) Save(value interface{}) *gorm.DB {
+	return g.DB.Save(value)
+}
+
+func (g *GormDatabase) Delete(value interface{}, where ...interface{}) *gorm.DB {
+	return g.DB.Delete(value, where...)
+}
+
+func InitDatabase() (*GormDatabase, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
@@ -20,9 +56,14 @@ func InitDatabase() {
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
+		return nil, err
 	}
+
+	DB := &GormDatabase{DB: db}
+
+	return DB, nil
 }
