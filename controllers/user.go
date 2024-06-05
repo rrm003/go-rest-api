51ee5ee5eb9fdf5a85ecd03ea6f07035b1a3b51c
rrm003 <rammankar003@gmail.com>
2024-06-05 13:54:48 -0500

    added swagger annotation

--- controllers/user.go
+++ controllers/user.go
@@ -6,8 +6,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"gorm.io/gorm"
 )
+
+var jwtKey = []byte("secret_key")
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 type UserController struct {
 	service services.UserService
@@ -17,6 +30,17 @@ func NewUserController(service services.UserService) *UserController {
 	return &UserController{service: service}
 }
 
+// SignUp creates a new user
+// @Summary Sign up a new user
+// @Description Create a new user with a username, password, and country
+// @Tags user
+// @Accept json
+// @Produce json
+// @Param user body models.User true "User to create"
+// @Success 200 {object} models.User
+// @Failure 400 {object} gin.H
+// @Failure 500 {object} gin.H
+// @Router /signup [post]
 func (ctrl *UserController) SignUp(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -32,6 +56,19 @@ func (ctrl *UserController) SignUp(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": user})
 }
 
+// Login user
+// Login logs in a user and returns a JWT token
+// @Summary Log in a user
+// @Description Authenticate a user and return a JWT token
+// @Tags user
+// @Accept json
+// @Produce json
+// @Param credentials body Credentials true "User credentials"
+// @Success 200 {object} gin.H
+// @Failure 400 {object} gin.H
+// @Failure 401 {object} gin.H
+// @Failure 500 {object} gin.H
+// @Router /login [post]
 func (ctrl *UserController) Login(c *gin.Context) {
 	var creds Credentials
 	if err := c.ShouldBindJSON(&creds); err != nil {
@@ -48,6 +85,16 @@ func (ctrl *UserController) Login(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
+// GetUsers returns all users
+// @Summary Get all users
+// @Description Get a list of all users
+// @Tags user
+// @Security BearerAuth
+// @Produce json
+// @Param Authorization header string true "Authorization token"
+// @Success 200 {array} models.User
+// @Failure 500 {object} gin.H
+// @Router /users [get]
 func (ctrl *UserController) GetUsers(c *gin.Context) {
 	users, err := ctrl.service.GetUsers()
 	if err != nil {
@@ -57,6 +104,19 @@ func (ctrl *UserController) GetUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": users})
 }
 
+// GetUser returns a user by ID
+// @Summary Get a user by ID
+// @Description Get a user by their ID
+// @Tags user
+// @Security BearerAuth
+// @Produce json
+// @Param Authorization header string true "Authorization token"
+// @Param id path int true "User ID"
+// @Success 200 {object} models.User
+// @Failure 400 {object} gin.H
+// @Failure 404 {object} gin.H
+// @Failure 500 {object} gin.H
+// @Router /users/{id} [get]
 func (ctrl *UserController) GetUser(c *gin.Context) {
 	id := c.Param("id")
 	user, err := ctrl.service.GetUser(id)
@@ -71,6 +131,21 @@ func (ctrl *UserController) GetUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": user})
 }
 
+// UpdateUser updates a user by ID
+// @Summary Update a user by ID
+// @Description Update a user's information by their ID
+// @Tags user
+// @Accept json
+// @Security BearerAuth
+// @Produce json
+// @Param Authorization header string true "Authorization token"
+// @Param id path int true "User ID"
+// @Param user body models.User true "Updated user information"
+// @Success 200 {object} models.User
+// @Failure 400 {object} gin.H
+// @Failure 404 {object} gin.H
+// @Failure 500 {object} gin.H
+// @Router /users/{id} [put]
 func (ctrl *UserController) UpdateUser(c *gin.Context) {
 	var user models.User
 	id := c.Param("id")
@@ -92,6 +167,19 @@ func (ctrl *UserController) UpdateUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": user})
 }
 
+// DeleteUser deletes a user by ID
+// @Summary Delete a user by ID
+// @Description Delete a user by their ID
+// @Tags user
+// @Security BearerAuth
+// @Produce json
+// @Param Authorization header string true "Authorization token"
+// @Param id path int true "User ID"
+// @Success 200 {object} gin.H
+// @Failure 400 {object} gin.H
+// @Failure 404 {object} gin.H
+// @Failure 500 {object} gin.H
+// @Router /users/{id} [delete]
 func (ctrl *UserController) DeleteUser(c *gin.Context) {
 	id := c.Param("id")
 
